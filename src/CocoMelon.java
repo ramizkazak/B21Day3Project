1d13e76cc9fc9d7c7be5278fc9c98c78c8921b61
ramizkazak <ramizkazak@gmail.com>
2020-11-12 10:32:33 -0600

    johnny johnny

--- src/CocoMelon.java
+++ src/CocoMelon.java
@@ -1,5 +1,6 @@
 public class CocoMelon {
     public static void main(String[] args) {
         System.out.println(" Are we there yey");
+        System.out.println("Johnny Johnny yes papa");
     }
 }
