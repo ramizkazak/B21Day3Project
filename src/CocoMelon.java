98be80c69f1c14034be284d465f74d11b8e400b9
ramizkazak <ramizkazak@gmail.com>
2020-11-12 10:33:01 -0600

    Jelly bean

--- src/CocoMelon.java
+++ src/CocoMelon.java
@@ -2,5 +2,6 @@ public class CocoMelon {
     public static void main(String[] args) {
         System.out.println(" Are we there yey");
         System.out.println("Johnny Johnny yes papa");
+        System.out.println("class pet, Jelly bean");
     }
 }
